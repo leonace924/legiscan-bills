e44f2f1021ec3078f835ab7f420f0b675e01c2dd
leonace924 <meobius123@gmail.com>
2023-09-01 09:27:29 -0400

    feat: add icons and update input field

---
+++ src/components/atom/icons/index.js
@@ -0,0 +1,35 @@
+export function IconArrowLeft() {
+  return (
+    <svg
+      aria-hidden="true"
+      focusable="false"
+      role="presentation"
+      viewBox="0 0 12 21"
+    >
+      <polygon
+        points="9.43 0 10.14 0.71 1.42 9.42 10.14 18.06 9.43 18.77 0 9.42 9.43 0"
+        fill={"currentColor"}
+        stroke={"currentColor"}
+        stroke-width={2}
+      ></polygon>
+    </svg>
+  );
+}
+
+export function IconArrowRight() {
+  return (
+    <svg
+      aria-hidden="true"
+      focusable="false"
+      role="presentation"
+      viewBox="0 0 12 21"
+    >
+      <polygon
+        points="1.8,19.9 1.1,19.2 9.8,10.5 1.1,1.9 1.8,1.2 11.2,10.5"
+        fill={"currentColor"}
+        stroke={"currentColor"}
+        stroke-width={2}
+      ></polygon>
+    </svg>
+  );
+}
--- src/components/atom/input/index.js
+++ src/components/atom/input/index.js
@@ -1,17 +1,27 @@
 import clsx from "clsx";
 
-const TextInput = ({ id, label, className, type, name, ...props }) => {
+const TextInput = ({
+  label,
+  className,
+  type,
+  name,
+  validation,
+  register,
+  errors,
+  ...props
+}) => {
   return (
     <div className="flex flex-col">
-      {label && <label htmlFor={id}>{label}</label>}
+      {label && <label htmlFor={name}>{label}</label>}
       <input
         className={clsx(
-          "flex-1 p-2 bg-gray-200/80 rounded-md shadow-inner border border-gray-500 text-sm",
-          className
+          "flex-1 p-2 bg-gray-200/80 rounded-md shadow-inner border border-gray-500 text-sm duration-300",
+          className,
+          errors && "border-red-500 focus:border-red-500 outline-red-500"
         )}
-        id={id}
         type={type}
         aria-label={name}
+        {...register(name, validation)}
         {...props}
       />
     </div>
